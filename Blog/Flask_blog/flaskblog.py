b18f7e404a7a212857c5732c888a38652a65b11e
Leon <leon.g.m@hotmail.co.uk>
2018-08-10 11:16:18 +0100

    Added my blog projects files and folders

---
+++ Blog/Flask_blog/flaskblog.py
@@ -0,0 +1,55 @@
+from flask import flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '0bdb52efcee96feafd52701447544c59'
+
+posts = [
+
+    {
+        'author': 'Leon Martin',
+        'title': 'Blog Post 2',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+
+    },
+
+    {
+        'author': 'Bob Martin',
+        'title': 'Blog Post 2',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+
+    }
+
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+       # flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
