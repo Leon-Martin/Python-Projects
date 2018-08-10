d5ece24069a6097ef780f1709d7169cb7dd9bf34
Leon Martin <37636743+Leon-Martin@users.noreply.github.com>
2018-08-10 11:13:50 +0100

    Delete Keywords.py

--- Keywords.py
+++
@@ -1,7 +0,0 @@
-def sentence(name='Leon',action='ate',food='pizza'):
-    print(name,action,food)
-
-sentence()
-sentence('dave','drank','orange juice')
-sentence(food='spagbol')
-sentence(food='burger',action='threw')
